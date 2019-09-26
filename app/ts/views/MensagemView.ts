222b17e899c5285087eca43dcfef34aa7db30418
vitorino <gui_vito@hotmail.com>
2019-09-25 22:30:37 -0300

    modules system

--- app/ts/views/MensagemView.ts
+++ app/ts/views/MensagemView.ts
@@ -1,9 +1,9 @@
-namespace Views {
-    export class MensagemView extends View<string> {
+import { View } from './View';
 
-        template(model: string): string {
+export class MensagemView extends View<string> {
 
-            return `<p class="alert alert-info">${model}</p>`;
-        }
+    template(model: string): string {
+
+        return `<p class="alert alert-info">${model}</p>`;
     }
 }
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,19 +1,17 @@
-namespace Views {
-    export abstract class View<T> {
+export abstract class View<T> {
 
-        private _elemento: JQuery;
+    private _elemento: JQuery;
 
-        constructor(seletor: string) {
+    constructor(seletor: string) {
 
-            this._elemento = $(seletor);
-        }
-
-        update(model: T) {
-
-            this._elemento.innerHTML = this.template(model);
-        }
-
-        abstract template(model: T): string;
-
+        this._elemento = $(seletor);
     }
+
+    update(model: T) {
+
+        this._elemento.innerHTML = this.template(model);
+    }
+
+    abstract template(model: T): string;
+
 }
